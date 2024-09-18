6e5670b979ff6bce97d748d67c090d63f1f2577c
Giancarlo <giancarlobianchi_12@hotmail.com>
2024-09-18 13:23:17 -0300

    Feature: Added oauth authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken(env('TOKEN_NAME', 'Laravel Personal Access Client'))->accessToken;
+
+            return response()->json(['token' => $token], Response::HTTP_OK);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+    }
+}
